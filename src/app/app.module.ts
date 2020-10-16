9818b688755f0cbd3628b9307369eadfea6102f0
Baishali Datta <baishali95920@gmail.com>
2020-10-17 00:32:35 +0530

    a

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 //import {FormsModule} from '@angular/forms';
-import { HttpClient, HttpParams } from '@angular/common/http';
+import { HttpClientModule } from '@angular/common/http';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { HomeComponent } from './home/home.component';
@@ -18,8 +18,7 @@ import { JobPostingComponent } from './job-posting/job-posting.component';
     BrowserModule,
     AppRoutingModule,
     CommonModule,
-    HttpClient,
-    HttpParams
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/job-posting/job-posting.component.ts
+++ src/app/job-posting/job-posting.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { HttpClient, HttpParams } from '@angular/common/http';
+import { HttpClient } from '@angular/common/http';
 // import { Injectable } from '@angular/core';
 import {FormsModule} from '@angular/forms';
 
