66f1648cb64408c69d96ab727b335d89015d7430
Baishali Datta <baishali95920@gmail.com>
2020-10-17 02:47:00 +0530

    a

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,18 +1,20 @@
 import { Component, OnInit } from '@angular/core';
-import { HttpClient, HttpParams } from "@angular/common/http";
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { FormBuilder, FormGroup } from '@angular/forms';
 import { CompleterService, CompleterData } from 'ng2-completer';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  styleUrls: ['./home.component.css'],
 })
 export class HomeComponent implements OnInit {
-
-  stateForm: FormGroup;
-
+  // stateForm: FormGroup;
+  searchStr: String;
+  captain: String;
   dataService: CompleterData;
+  input: String;
 
   protected searchData = [
     { color: 'red', value: '#f00' },
@@ -21,17 +23,30 @@ export class HomeComponent implements OnInit {
     { color: 'cyan', value: '#0ff' },
     { color: 'magenta', value: '#f0f' },
     { color: 'yellow', value: '#ff0' },
-    { color: 'black', value: '#000' }
+    { color: 'black', value: '#000' },
   ];
-  protected captains = ['James T. Kirk', 'Benjamin Sisko', 'Jean-Luc Picard', 'Spock', 'Jonathan Archer', 'Hikaru Sulu', 'Christopher Pike', 'Rachel Garrett' ];
-
-  constructor( private fb: FormBuilder, private completerService: CompleterService) {
-    this.dataService = completerService.local(this.searchData, 'color', 'color');
+  protected captains = [
+    'James T. Kirk',
+    'Benjamin Sisko',
+    'Jean-Luc Picard',
+    'Spock',
+    'Jonathan Archer',
+    'Hikaru Sulu',
+    'Christopher Pike',
+    'Rachel Garrett',
+  ];
+  
+  constructor(completerService: CompleterService) {
+    this.dataService = completerService.local(
+      this.searchData,
+      'color',
+      'color'
+    );
   }
 
+  ngOnInit() {}
 
- ngOnInit() {
-  
- }
-
+  searchJob(){
+    console.log("input is", input)
+  }
 }
