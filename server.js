cd70914a013e3aca8d55810a68a491a459db0524
Baishali Datta <baishali95920@gmail.com>
2020-10-16 18:47:38 +0530

    mlab

--- server.js
+++ server.js
@@ -54,8 +54,8 @@
 
 var express = require("express");
 var bodyParser = require("body-parser");
-var mongodb = require("mongodb");
-var ObjectID = mongodb.ObjectID;
+// var mongodb = require("mongodb");
+// var ObjectID = mongodb.ObjectID;
 
 var CONTACTS_COLLECTION = "contacts";
 
@@ -63,30 +63,32 @@ var app = express();
 app.use(bodyParser.json());
 
 // Create a database variable outside of the database connection callback to reuse the connection pool in your app.
-var db;
-
+//var db;
+var bleh = "mongodb://ouchpotato2020@gmail.com:Baishali123!@ds01316.mlab.com:1316/food"
 // Connect to the database before starting the application server.
 // mongodb.MongoClient.connect(process.env.MONGODB_URI || "mongodb://localhost:27017/test", function (err, client) {
-//   if (err) {
-//     console.log(err);
-//     process.exit(1);
-//   }
 
-//   // Save database object from the callback for reuse.
-//   db = client.db();
-//   console.log("Database connection ready");
+    mongodb.MongoClient.connect(bleh || "mongodb://localhost:27017/test", function (err, client) {
+  if (err) {
+    console.log(err);
+    process.exit(1);
+  }
 
-//   // Initialize the app.
-//   var server = app.listen(process.env.PORT || 8080, function () {
-//     var port = server.address().port;
-//     console.log("App now running on port", port);
-//   });
-// });
+  // Save database object from the callback for reuse.
+  db = client.db();
+  console.log("Database connection ready");
 
-var server = app.listen(process.env.PORT || 8080, function () {
+  // Initialize the app.
+  var server = app.listen(process.env.PORT || 8080, function () {
     var port = server.address().port;
     console.log("App now running on port", port);
   });
+});
+
+// var server = app.listen(process.env.PORT || 8080, function () {
+//     var port = server.address().port;
+//     console.log("App now running on port", port);
+//   });
 
 
 
@@ -118,15 +120,15 @@ function handleError(res, reason, message, code) {
        })
    })
   
-//   app.get("/api/contacts", function(req, res) {
-//     db.collection(CONTACTS_COLLECTION).find({}).toArray(function(err, docs) {
-//       if (err) {
-//         handleError(res, err.message, "Failed to get contacts.");
-//       } else {
-//         res.status(200).json(docs);
-//       }
-//     });
-//   });
+  app.get("/api/contacts", function(req, res) {
+    db.collection(CONTACTS_COLLECTION).find({}).toArray(function(err, docs) {
+      if (err) {
+        handleError(res, err.message, "Failed to get contacts.");
+      } else {
+        res.status(200).json(docs);
+      }
+    });
+  });
   
 //   app.post("/api/contacts", function(req, res) {
 //     var newContact = req.body;
