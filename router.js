4d50bf11db8e3081c06aa828c8da276e8e9070fb
Baishali Datta <baishali95920@gmail.com>
2020-10-17 05:55:25 +0530

    a

--- router.js
+++ router.js
@@ -3,7 +3,7 @@ const controller = require('./controller');
 
 router.get('/signup', controller.signUp);
 router.post('/job', controller.createJob);
-router.get('/jobs', controller.getJobs);
+// router.get('/jobs', controller.getJobs);
 router.get('/filter', controller.getFilter);
 // router.get('/joblist', controller.jobList);
 
